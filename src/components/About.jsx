45f5e39a84eb59f710ce6f46e42cc95c78b034da
Cootzy <umastryxsubterror@gmail.com>
2025-06-28 00:58:57 +0700

    Implement Animated Title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,10 @@ const About = () => {
           Welcome to Rico`s World
         </h2>
 
-        <AnimatedTitle />
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the World`s <br /> L<b>a</b>rgest Shared Adventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         {/* <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
           Disc<b>o</b>ver the World`s <br /> L<b>a</b>rgest Shared Adventure
